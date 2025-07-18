5f1c7c17a35adb961201bb77467917873161dedd
driver4567 <ryan.mckechnie@gmail.com>
2025-07-18 12:03:20 +1000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -219,18 +219,21 @@ function initScrollEffects() {
   // Smooth scrolling for anchor links
   const anchorLinks = document.querySelectorAll('a[href^="#"]:not([href="#"])');
   anchorLinks.forEach(link => {
-    link.addEventListener('click', function(e) {
-      e.preventDefault();
-      
-      const targetId = this.getAttribute('href').substring(1);
-      const targetElement = document.getElementById(targetId);
-      
-      if (targetElement) {
-        targetElement.scrollIntoView({
-          behavior: 'smooth',
-          block: 'start'
-        });
-      }
+    //link.addEventListener('click', function(e) {
+    ['click', 'touchend'].forEach(eventType => {    //touch added for iOS iphone event
+      link.addEventListener(eventType, function(e) {
+        e.preventDefault();
+        
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        if (targetElement) {
+          targetElement.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+          });
+        }
+      });
     });
   });
 }
